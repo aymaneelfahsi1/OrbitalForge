059da3685986bc336936b53aceb43dd83836e58f
aymane.elfahsi1 <aymane.elfahsi1@gmail.com>
2026-07-20 16:33:14 +0100

    Day 2

--- build-release/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aymane/Desktop/OrbitalForge
-# Build directory: /home/aymane/Desktop/OrbitalForge/build-release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/aymane/Desktop/OrbitalForge/build-release/orbitalforge_tests-b12d07c_include.cmake")
